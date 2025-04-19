952a21d42fd94de07e80ad45733dc57149006be0
gpmakm <165992407+gpmakm@users.noreply.github.com>
2025-04-19 17:21:48 +0530

    Update Schema.js

--- src/app/api/models/Schema.js
+++ src/app/api/models/Schema.js
@@ -1,15 +1,12 @@
 import mongoose from "mongoose";
 
-const TemplateSchema = new mongoose.Schema(
-  {
-    display_pic: { type: String, required: true },
-    template: { type: String, required: true },
-    price: { type: Number, required: true },
-    cardType: {type:String,required:true},
-    description: { type: String, required: true },
-    salesCount: { type: Number, default: 0 }, // ✅ New field to track sales
-  },
-  { timestamps: true }
-);
+const TemplateSchema = new mongoose.Schema({
+  display_pic: { type: String, required: true },
+  template: { type: String, required: true },
+  price: { type: Number, required: true },
+  cardType: { type: String, required: true },
+  description: { type: String, required: true },
+  salesCount: { type: Number, default: 0 },
+}, { timestamps: true });
 
 export default mongoose.models.Template || mongoose.model("Template", TemplateSchema);
